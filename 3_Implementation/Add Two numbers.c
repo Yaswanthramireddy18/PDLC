7baf5a2a691a646494f4e1138e3a09d38466844d
saipreetham <145094515+saipreethamsanda@users.noreply.github.com>
2024-05-17 14:32:53 +0530

    Update Add Two numbers.c

--- 3_Implementation/Add Two numbers.c
+++ 3_Implementation/Add Two numbers.c
@@ -5,7 +5,7 @@ int main()
     int result;
     printf("Enter two numbers:");
     scanf("%d %d",&num1,&num2);
-    result=num1+num2;
+    result=num1*num2;
     printf("Sum of %d and %d is %d\n",num1,num2,result);
     return 0;
     
